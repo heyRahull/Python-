57498543c69947abfa838c047405e4902c421772
Rahul Agarwal <rahul123agarwal99@gmail.com>
2020-04-09 12:43:41 +0530

    Exercise-4

---
+++ beginner/DisplayCharactersAtEvenPlace.py
@@ -0,0 +1,13 @@
+# Question 3: Accept string from a user and 
+# display only those characters which are present at an even index number.
+
+# For example str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+
+str1 = input("Enter a String")
+print("THe original string is :", str1)
+
+print("The output is :")
+for x in range(0, len(str1)-1, 2):
+    print("index[", x, "]", str1[x])
+
+
