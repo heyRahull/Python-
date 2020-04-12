8bb9a53e5e9cc18e1814573f04e437c4ed860336
Rahul Agarwal <rahul123agarwal99@gmail.com>
2020-04-12 08:34:16 +0530

    Exercise-7

---
+++ beginner/No.DivisibleBy5.py
@@ -0,0 +1,6 @@
+# Question 6: Given a list of numbers, Iterate it and print only those numbers which are divisible of 5
+
+list1 =[10,20,33,46,55]
+for x in list1:
+    if(x%5 == 0):
+        print(x)
