34b9ab44c37d07ec1bf906fd32e7473bdc3a772e
Rahul Agarwal <rahul123agarwal99@gmail.com>
2020-04-12 08:59:27 +0530

    Exercise-10

---
+++ beginner/ReverseTheNumber.py
@@ -0,0 +1,16 @@
+# Question 9: Reverse a given number and return true if it is the same as the original number
+
+def reverseCheck(number):
+    print("original number", number)
+    originalNum = number
+    reverseNum = 0
+    while (number > 0):
+        reminder = number % 10
+        reverseNum = (reverseNum * 10) + reminder
+        number = number // 10
+    if (originalNum == reverseNum):
+        return True
+    else:
+        return False
+
+print("The original and reverse number is the same:", reverseCheck(121))
